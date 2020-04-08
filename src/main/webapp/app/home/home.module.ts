84e22d7b66352fd86fcebe47701c66cf77f5247e
Amine Troudi <mohamedamine.troudi@outlook.com>
2020-04-08 19:43:53 +0200

    front: add edit Volunteer multiple services - admin login in footer

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { StayHomeSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
-  imports: [StayHomeSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [StayHomeSharedModule, RouterModule.forChild([HOME_ROUTE]), NgSelectModule],
   declarations: [HomeComponent]
 })
 export class StayHomeHomeModule {}
